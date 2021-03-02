706190d1bf0da1ff4731fc94e5ee8bb821b35ccf
Suelen dos Santos Batista <suelen.batista@al.infnet.edu.br>
2021-03-02 20:45:54 -0300

    ultimo do dia

---
+++ src/components/layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Layout = () => {
+    return(
+        <div>
+            <header>
+                <h1>Nome do curso</h1>
+            </header>
+            <main>
+
+            </main>
+            <footer>
+                <p>Todos os direitos reservados</p>
+            </footer>
+        </div>
+    )
+};
+
+export default Layout
